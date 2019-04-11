260bc9a16f0cd360aed9b4516447dc5f22ab5743
israel <israel.bejarano.a@gmail.com>
2019-04-11 23:30:40 +0200

    solved tslint warnings

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -2,7 +2,7 @@ import { Action } from '@ngrx/store';
 import { INCREMENTAR, DECREMENTAR } from './contador.actions';
 
 export function contadorReducer(state: number = 10, action: Action) {
-    switch(action.type) {
+    switch (action.type) {
         case INCREMENTAR:
             return state + 1;
         case DECREMENTAR:
