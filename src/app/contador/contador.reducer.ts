8bca656e4c862dcb3fb98adaa10dcb937e736a87
israel <israel.bejarano.a@gmail.com>
2019-04-15 18:42:02 +0200

    code optimizacion

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,16 +1,16 @@
-import { actions, INCREMENTAR, DECREMENTAR, MULTIPLICAR, DIVIDIR, RESET } from './contador.actions';
+import * as fromContador from './contador.actions';
 
-export function contadorReducer(state: number = 10, action: actions) {
+export function contadorReducer(state: number = 10, action: fromContador.actions) {
     switch (action.type) {
-        case INCREMENTAR:
+        case fromContador.INCREMENTAR:
             return state + 1;
-        case DECREMENTAR:
+        case fromContador.DECREMENTAR:
             return state - 1;
-        case MULTIPLICAR:
+        case fromContador.MULTIPLICAR:
             return state * action.payload;
-        case DIVIDIR:
+        case fromContador.DIVIDIR:
             return state / action.payload;
-        case RESET:
+        case fromContador.RESET:
             return state = 0;
         default:
             return state;
