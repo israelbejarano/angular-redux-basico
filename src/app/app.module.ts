125822e09c96f7ea08b18bac8de2eb9f0c1389d4
israel <israel.bejarano.a@gmail.com>
2019-04-15 17:38:09 +0200

    solved error con la version de ngrx-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+// redux
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { contadorReducer } from './contador/contador.reducer';
 
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ contador: contadorReducer })
+    StoreModule.forRoot({ contador: contadorReducer }),
+    // sirve para con la extension en chrome ir depurando y ver los estados anteriores (25) solo en dev en prod no se ve
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
